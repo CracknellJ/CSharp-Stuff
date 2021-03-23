306abdbc234cf387a0497682b8a38a1dfb1284e5
Jack Cracknell <jcracknell123@gmail.com>
2021-03-23 17:14:07 +0000

    Initial C# folder with a hello world class

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Stuff
+{
+   class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
